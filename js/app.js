4bdc3bcdf8448d5f5796cc4a0e8bcf3533528918
Tomas Sabbavini <tomassabbavini@hotmail.com>
2021-12-16 22:35:03 -0300

    c

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("ok");
+const cuadros = document.querySelectorAll('.cuadro');
+const topo = document.querySelector('.topo');
+const tiempoRestante = document.querySelector('#tiempoRestante')
+const puntaje = document.querySelector('#puntaje')
+
+let resultado = 0;
+
+function randomCuadro () {
+    cuadros.forEach(cuadro => {
+        cuadro.classList.remove('topo');
+    })
+
+    let randomPosicion = cuadros[Math.floor(Math.random() * 9)]
+}
+
+randomCuadro();

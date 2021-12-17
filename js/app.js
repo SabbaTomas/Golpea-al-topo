61ee0b1ff241eff1e618865046c35ab5c9aa1e16
Tomas Sabbavini <tomassabbavini@hotmail.com>
2021-12-16 23:05:44 -0300

    cambios

--- js/app.js
+++ js/app.js
@@ -4,13 +4,44 @@ const tiempoRestante = document.querySelector('#tiempoRestante')
 const puntaje = document.querySelector('#puntaje')
 
 let resultado = 0;
+let hitPostition
+let tiempoTotal = 10;
 
 function randomCuadro () {
     cuadros.forEach(cuadro => {
         cuadro.classList.remove('topo');
     })
 
-    let randomPosicion = cuadros[Math.floor(Math.random() * 9)]
+    let randomCuadro = cuadros[Math.floor(Math.random() * 9)]
+    randomCuadro.classList.add('topo')
+
+    hitPostition = randomCuadro.id;
 }
 
-randomCuadro();
+cuadros.forEach(cuadro => {
+    cuadro.addEventListener('mousedown', () => {
+        if(cuadro.id == hitPostition) {
+            resultado++
+            puntaje.textContent = resultado
+            hitPostition = null
+        }
+    })
+})
+
+function moverTopo () {
+    let timerId = null
+    timerId = setInterval(randomCuadro, 1000)
+}
+moverTopo();
+
+function contador () {
+    tiempoTotal--
+    tiempoRestante.textContent = tiempoTotal
+
+    if(tiempoTotal == 0){
+        clearInterval(Temporadizador)
+        alert("Gamer Over! tu resultado es: " + resultado)
+    }
+}
+
+let Temporadizador = setInterval(contador, 1000)
